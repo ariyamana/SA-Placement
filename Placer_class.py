2d83284497637d1da03dd0a8f799c4275263ee0b
Arman Zaribafiyan <armanzaribafiyan@Armans-MacBook-Pro.local>
2017-02-12 12:19:24 -0800

    the basics of simulated annealing are ready.

--- Placer_class.py
+++ Placer_class.py
@@ -1,5 +1,42 @@
 ''' This module defines a placer class that is designed based on simulated
 annealing optimization of a half-parameter bounding box cost function.'''
+import random as RND
+from numpy import exp
 
 class sa_placer_agent():
-    
+    def __init__(self, chip_obj, initial_T, max_iterations):
+        self.temp_schedule = []
+        self.max_iter = max_iterations
+        self.T = initial_T
+        self.num_ietr = 0
+        self.chip = chip_obj
+
+    def random_swap(self):
+
+        [i,j] = RND.sample(range(self.chip.num_cells),2)
+
+        delta_cost = self.chip.swap_delta_cost(i,j)
+
+        return i,j, delta_cost
+
+    def make_move(self):
+
+        cell_i, cell_j, delta_cost = self.random_swap()
+
+        if delta_cost < 0 :
+            self.chip.swap_cells(cell_i,cell_j)
+            self.T = self.T*.95
+        else:
+            if RND.uniform(0, 1) <= exp(-delta_cost/self.T):
+                self.chip.swap_cells(cell_i,cell_j)
+                self.T = self.T*.95
+            else:
+                self.T = self.T*.95
+    def anneal(self):
+
+        while self.num_ietr <= self.max_iter:
+            print 'iteration:', self.num_ietr,
+            self.make_move()
+
+            self.num_ietr += 1
+            print 'done with cost' , self.chip.total_cost
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import load as LD
+import Placer_class as PLC
 
 input_adress = 'Examples/cm138a.txt'
 chip1 = LD.load_input(input_adress, verbose =1)
@@ -12,3 +13,8 @@ else:
     print 'the cost is: ', chip1.swap_delta_cost(0,10)
 
 chip1.display()
+
+sa_agent = PLC.sa_placer_agent(chip1, 100, 1000)
+
+
+sa_agent.anneal()
