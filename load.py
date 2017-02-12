ec61073f50ca459c9dffad769f1bb73dd8fe0679
Arman Zaribafiyan <armanzaribafiyan@Armans-MacBook-Pro.local>
2017-02-11 21:41:05 -0800

    the module opens the input file.

--- load.py
+++ load.py
@@ -1,2 +1,15 @@
 ''' This module imports the input information from an input file.'''
 
+def load_input(filename):
+    f = open(filename, "r")
+
+    file_content = f.readlines()
+
+    print file_content
+
+
+if __name__ == "__main__":
+
+    input_adress = 'Examples/cm138a.txt'
+
+    load_input(input_adress)
